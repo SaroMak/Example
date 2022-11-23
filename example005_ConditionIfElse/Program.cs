ba2c51497aa5eab0f428e3f225d3aac3e0a3db48
«Saro» <opac4321@mail.ru>
2022-11-23 20:28:30 +0300

    example005 if else

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Имя пользывателя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("супер,это Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
