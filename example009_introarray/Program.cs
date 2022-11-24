28cbcba2282f847a4265040ebe7df42e27622d95
«Saro» <opac4321@mail.ru>
2022-11-24 16:51:40 +0300

    не доделанный метод 24 минута семинар2

---
+++ example009_introarray/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max( int arg1, int arg2, int arg3)
+
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+} 
+
+int[] array = {12, 23, 445, 23, 54, 26, 98, 9878, 6454, 34};
+array[0] = 12;
+
+Console.WriteLine(array[6]);
